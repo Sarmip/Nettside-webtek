ea80170533170fce2c1a45ba57f74138480da079
keithcharlene <54809082+keithcharlene@users.noreply.github.com>
2019-11-01 13:34:48 +0100

    section i samme posisjon selv etter valg

--- tips-test.js
+++ tips-test.js
@@ -2,11 +2,10 @@ function visGruppe() {
     var valgt = document.getElementById("tips_valg")
     var kategori = valgt.options[valgt.selectedIndex].value // Hvilken kategori som har blitt valgt
 
-    // FIKS: Bildene havner under når man velger en kategori
     var sections = document.getElementsByTagName("section")
     for (i = 0; sections.length; i++) {
         if (sections[i].classList.contains(kategori)) {
-            sections[i].style.display = "inline-flex"
+            sections[i].style.display = "flex"
         } else {
             sections[i].style.display = "none"
         }
